e95807390bd0f39028b53db8adbd58b68e4d9166
YakshitaRathod <72180197+YakshitaRathod@users.noreply.github.com>
2020-10-03 04:19:19 +0530

    Update square root.py

--- square root.py
+++ square root.py
@@ -1,7 +1,4 @@
-#Python program to calculate the square root
-
-#Take the input form the user to give the number.
 num=float(input("Enter the number: "))
 
 num_sqrt = num**0.5
-print("The square root of the number you enterd"%(num ,num_sqrt))
+print("The square root of the number you enterd",num_sqrt)
